4111225d67c910bfd98a3536a4e20963cea80722
tylercbreton <tcbreton@gmail.com>
2014-06-09 16:04:03 -0700

    Finish user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,74 +1,53 @@
 require 'spec_helper'
 
-describe User do
-<<<<<<< HEAD
-  pending "add some examples to (or delete) #{__FILE__}"
-end
-=======
-  
-  before do
-    @user = User.new(name: "Example User", email: "user@example.com",
-                    password: "foobar", password_confirmation: "foobar")
+describe "User pages" do
+
+  subject { page }
+
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
   end
 
-  subject { @user }
+  describe "signup page" do
+    before { visit signup_path }
 
-  it { should respond_to(:name) }
-  it { should respond_to(:email) }
-  it { should respond_to(:password_digest) }
-  it { should respond_to(:password) }
-  it { should respond_to(:password_confirmation) }
-  it { should be_valid }
-  it { should respond_to(:authenticate) }
-
-
-  describe "when name is not present" do
-    before { @user.name = " " }
-    it { should_not be_valid }
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
   end
-  
-   describe "when email is not present" do
-    before { @user.email = " " }
-    it { should_not be_valid }
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
   end
 
-   describe "when name is too long" do
-    before { @user.name = "a" * 51 }
-    it { should_not be_valid }
-  end
+  describe "signup" do
 
-  describe "when password is not present" do
-    before do
-      @user = User.new(name: "Example User", email: "user@example.com",
-                     password: " ", password_confirmation: " ")
+    before { visit signup_path }
+
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
     end
-    it { should_not be_valid }
-  end
 
-  describe "when password doesn't match confirmation" do
-    before { @user.password_confirmation = "mismatch" }
-    it { should_not be_valid }
-  end
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
 
-  describe "with a password that's too short" do
-    before { @user.password = @user.password_confirmation = "a" * 5 }
-    it { should be_invalid }
-  end
-
-  describe "return value of authenticate method" do
-  before { @user.save }
-  let(:found_user) { User.find_by(email: @user.email) }
-
-  describe "with valid password" do
-    it { should eq found_user.authenticate(@user.password) }
-  end
-
-  describe "with invalid password" do
-    let(:user_for_invalid_password) { found_user.authenticate("invalid") }
-
-    it { should_not eq user_for_invalid_password }
-    specify { expect(user_for_invalid_password).to be_false }
-  end
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end 
 end
-end
->>>>>>> modeling-users
